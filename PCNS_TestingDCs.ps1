124fb4ed11a40b88558eba9ffc8fd273194fbba2
stbadger <77111154+stbadger@users.noreply.github.com>
2025-08-11 13:03:02 -0400

    Update PCNS_TestingDCs.ps1

--- PCNS_TestingDCs.ps1
+++ PCNS_TestingDCs.ps1
@@ -1,3 +1,5 @@
+Import-Module ActiveDirectory
+
 # Establishes list of domain controllers that will be tested.
 $domainName = Read-Host "Enter your domain name"
 $DCs = Get-ADDomainController -discover -domain $domainName | Select-Object -ExpandProperty HostName
@@ -25,12 +27,8 @@ param($registryPath, $registryValueName, $registryValueData, $username)
     Write-Host "Conducting PCNS Validation Testing on DC: $env:COMPUTERNAME IP: $IP" -ForegroundColor DarkYellow
     Write-Output "$env:COMPUTERNAME,$IP"
 
-    # Creates required registry path for PCNS verbose logging if it does not exist.
-    try {
-        if (-not (Test-Path $registryPath)) {
-            New-Item -Path $registryPath -Force
-            Write-Host "New registry path to enable PCNS logging created"
-        }
+    # Only conducts PCNS validation testing if the PCNS service registry path exists.
+    if (Test-Path $registryPath) {
 
         # Sets the registry value to enable PCNS verbose logging.
         try {
@@ -56,71 +54,72 @@ param($registryPath, $registryValueName, $registryValueData, $username)
         } catch {
             Write-Host "An error occurred updating the registry value to enable PCNS verbose logging" -ForegroundColor Red
         }
-    } catch {
-        Write-Host "An error occurred creating the registry path to enable PCNS logging" -ForegroundColor Red
-    }
     
-    # Pauses the script to give time for the PCNS service to start before conducting a password reset.
-    Start-Sleep -Seconds 5
-
-    # Establishes a random secure password to be used for resetting the user's password.
-    Add-Type -AssemblyName System.Web
-    $newPassword = [System.Web.Security.Membership]::GeneratePassword(16, 4)
-    $securePassword = ConvertTo-SecureString $newPassword -AsPlainText -Force
-
-    try {
-        # Records time to use when filtering event codes.
-        $resetTime = Get-Date
-        $resetTime = $resetTime.AddMinutes(-1)
-
-        # Resets user's password.
-        Set-ADAccountPassword -Identity $username -NewPassword $securePassword
-        Set-ADUser -Identity $username -PasswordNeverExpires $false
-        Write-Host "Password for user $username has been reset"
-
-        # Pauses script to allow event codes to populate.
+        # Pauses the script to give time for the PCNS service to start before conducting a password reset.
         Start-Sleep -Seconds 5
 
-        # Checks event log for identified PCNS events and outputs test results.
-        try {
-            $Events = Get-EventLog -LogName Application | Where-Object { 
-                $_.Source -eq "PCNSSVC" -and $_.TimeGenerated -gt $resetTime
-            }
-            if ($Events) {
-                    Write-Output "Time,Event ID,Description"
-                    $Events | ForEach-Object { 
-                        $message = $_.Message -split "`n" | Select-Object -First 1
-                        if ($_.EntryType -eq "Information") {
-                            Write-Output "$($_.TimeGenerated),$($_.EventId),$($message.Trim())"
-                            if ($_.EventID -eq "2100") {
-                                Write-Host "Time: $($_.TimeGenerated), Event ID: $($_.EventId), $($message.Trim())" -ForegroundColor Green
-                            }
-                        } elseif ($_.EntryType -eq "Warning") {
-                            Write-Output "$($_.TimeGenerated),$($_.EventId),$($message.Trim())"
-                        } elseif ($_.EntryType -eq "Error") {
-                            Write-Output "$($_.TimeGenerated),$($_.EventId),$($message.Trim())"
-                        }
-                    }
-            } else {
-                Write-Output "No relevant event codes were found."
-            }
-        } catch {
-            Write-Output "An error occurred while checking the Event Viewer:,$_"
-        }
-    } catch {
-        Write-Output "An error occurred while resetting the password:,$_"
-    }
+        # Establishes a random secure password to be used for resetting the user's password.
+        Add-Type -AssemblyName System.Web
+        $newPassword = [System.Web.Security.Membership]::GeneratePassword(16, 4)
+        $securePassword = ConvertTo-SecureString $newPassword -AsPlainText -Force
 
-    # Resets registry value to disable PCNS verbose logging.
-    $loggingValueData = Get-ItemPropertyValue -Path $registryPath -Name $registryValueName
-    if ($loggingValueData -eq "3"){
-        try{
-            Set-ItemProperty -Path $registryPath -Name $registryValueName -Value "1"
-            Write-Host "Registry value updated successfully to disable PCNS verbose logging"
+        try {
+            # Records time to use when filtering event codes.
+            $resetTime = Get-Date
+            $resetTime = $resetTime.AddMinutes(-1)
+
+            # Resets user's password.
+            Set-ADAccountPassword -Identity $username -NewPassword $securePassword
+            Set-ADUser -Identity $username -PasswordNeverExpires $false
+            Write-Host "Password for user $username has been reset"
+
+            # Pauses script to allow event codes to populate.
+            Start-Sleep -Seconds 5
+
+            # Checks event log for identified PCNS events and outputs test results.
+            try {
+                $Events = Get-EventLog -LogName Application | Where-Object { 
+                    $_.Source -eq "PCNSSVC" -and $_.TimeGenerated -gt $resetTime
+                }
+                if ($Events) {
+                        Write-Output "Time,Event ID,Description"
+                        $Events | ForEach-Object { 
+                            $message = $_.Message -split "`n" | Select-Object -First 1
+                            if ($_.EntryType -eq "Information") {
+                                Write-Output "$($_.TimeGenerated),$($_.EventId),$($message.Trim())"
+                                if ($_.EventID -eq "2100") {
+                                    Write-Host "Time: $($_.TimeGenerated), Event ID: $($_.EventId), $($message.Trim())" -ForegroundColor Green
+                                }
+                            } elseif ($_.EntryType -eq "Warning") {
+                                Write-Output "$($_.TimeGenerated),$($_.EventId),$($message.Trim())"
+                            } elseif ($_.EntryType -eq "Error") {
+                                Write-Output "$($_.TimeGenerated),$($_.EventId),$($message.Trim())"
+                            }
+                        }
+                } else {
+                    Write-Output "No relevant event codes were found."
+                }
+            } catch {
+                Write-Output "An error occurred while checking the Event Viewer:,$_"
+            }
         } catch {
-            Write-Host "An error occurred updating the registry value to disable PCNS verbose logging"
+            Write-Output "An error occurred while resetting the password:,$_"
         }
+
+        # Resets registry value to disable PCNS verbose logging.
+        $loggingValueData = Get-ItemPropertyValue -Path $registryPath -Name $registryValueName
+        if ($loggingValueData -eq "3"){
+            try{
+                Set-ItemProperty -Path $registryPath -Name $registryValueName -Value "1"
+                Write-Host "Registry value updated successfully to disable PCNS verbose logging"
+            } catch {
+                Write-Host "An error occurred updating the registry value to disable PCNS verbose logging"
+            }
                 
+        }
+    } else {
+        Write-Host "There was an error finding the PCNS service registry path."
+        Write-Output "There was an error finding the PCNS service registry path."
     }
 
     Write-Output ""
